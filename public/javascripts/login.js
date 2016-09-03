737b9ce0a0ba34e359ae7f7bced8b5f5ac462e6e
muxiao <muxiao@kingdowin.com>
2016-09-03 16:56:19 +0800

    shan

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,10 +1,4 @@
-/**
- * Created with JetBrains WebStorm.
- * User: 灵勇
- * Date: 13-8-13
- * Time: 下午9:02
- * To change this template use File | Settings | File Templates.
- */
+
 $(function(){
     cover();
 });
